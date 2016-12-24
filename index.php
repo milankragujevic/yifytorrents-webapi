61567510f271770da8f85d149b7446f9035b7b06
Milan Kragujević <milankragujevic@gmail.com>
2016-12-24 09:32:55 +0100

    fix up api

--- index.php
+++ index.php
@@ -9,6 +9,34 @@ function _cg($url) {
 	return $api;
 }
 
+/*Array
+(
+    [movie_results] => Array
+        (
+            [0] => Array
+                (
+                    [adult] => 
+                    [backdrop_path] => /geaAzUv09q5KaM9fHW74nIHeOCi.jpg
+                    [genre_ids] => Array
+                        (
+                            [0] => 35
+                        )
+
+                    [id] => 369883
+                    [original_language] => en
+                    [original_title] => Middle School: The Worst Years of My Life
+                    [overview] => A quiet teenage artist Rafe Katchadorian has a wild imagination and is sick of middle school and the rules that have been put before him. Rafe and his best friend Leo have come up with a plan: break every rule in the school hand book and as you expect trouble follows.
+                    [release_date] => 2016-10-07
+                    [poster_path] => /4ivn2Ze0kWpFNXgow2itfkdQftT.jpg
+                    [popularity] => 1.960159
+                    [title] => Middle School: The Worst Years of My Life
+                    [video] => 
+                    [vote_average] => 5.5
+                    [vote_count] => 6
+                )
+
+        )*/
+
 $tmdb_key = 'd763549ca4b13a17f5ef6ff914033fad';
 
 set_time_limit(0);
@@ -40,18 +68,19 @@ foreach($yts['data']['movies'] as $item) {
 	$torrent = []; foreach($item['torrents'] as $_t) { if($_t['quality'] == '720p') { $torrent = $_t; } }
 	$imdbID = $item['imdb_code'];
 	$tmdb_find = _cg('https://api.themoviedb.org/3/find/' . $imdbID . '?api_key=' . $tmdb_key . '&language=en-US&external_source=imdb_id');
-	print_r($tmdb_find); exit;
-	exit;
-	$movies[] = [
-		'title' => $item['title'],
+	$tmdb = $tmdb_find['movie_results'][0];
+	$movies[] = array(
+		'title' => $tmdb['title'],
 		'year' => $item['year'],
-		'background' => $item['background_image'],
-		'poster' => str_replace('medium', 'large', $item['medium_cover_image']),
+		'released_date' => date('F jS, Y', strtotime(
+		'background' => 'https://image.tmdb.org/t/p/original' . $tmdb['backdrop_path'],
+		'poster' => 'https://image.tmdb.org/t/p/w300' . $item['poster_path'],
 		'genres' => implode(', ', $item['genres']),
 		'rating' => $item['rating'],
 		'runtime' => $item['runtime'] . ' mins',
 		'quality' => '720p',
-		'infoHash' => $torrent['hash']
-	];
+		'infoHash' => $torrent['hash'],
+		'seeds' => $torrent['seeds']
+	);
 }
 echo json_encode($movies, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
