9c536526270508d664a301e2cdbc3144ca1b3a67
Milan Kragujević <milankragujevic@gmail.com>
2016-12-24 09:13:14 +0100

    improve API

--- index.php
+++ index.php
@@ -4,13 +4,26 @@ error_reporting(0);
 $page = (int)$_GET['page'];
 $genre = rawurldecode($_GET['genre']);
 $query = rawurldecode($_GET['query']);
-if(empty($genre)) { $genre = 'All'; }
 $genre = ucwords(str_replace('-', ' ', $genre));
 if($page < 1) { $page = 1; }
+$limit = 25;
+if(isset($_GET['limit'])) {
+	$limit = (int) $_GET['limit'];
+	if($limit > 50) { $limit = 50; }
+	if($limit < 25) { $limit = 25; }
+}
+$sort_by = 'views';
+if(isset($_GET['sort_by'])) {
+	$sort_by = $_GET['sort_by'];	
+}
+$sort_order = 'desc';
+if(isset($_GET['sort_order'])) {
+	$sort_order = $_GET['sort_order'];	
+}
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 if(!is_dir(__DIR__ . '/cache/')) { mkdir(__DIR__ . '/cache/'); }
-$url = 'https://yts.ag/api/v2/list_movies.json?limit=25&quality=720p&sort_by=seeds&order_by=desc&query_term=' . urlencode($query) . '&genre=' . urlencode($genre) . '&page=' . $page;
+$url = 'https://yts.ag/api/v2/list_movies.json?limit=' . $limit . '&quality=720p&sort_by=' . $sort_by . '&order_by=' . $sort_order . '&query_term=' . urlencode($query) . '&genre=' . urlencode($genre) . '&page=' . $page;
 $cache = __DIR__ . '/cache/' . sha1($url) . '.json';
 if(!is_file($cache) || time() - filemtime($cache) > 60 * 60) {
   file_put_contents($cache, file_get_contents($url));
